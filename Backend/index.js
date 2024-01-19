359ba408ec0cac62ac594097ef5d773605fb4064
imkhushboo <vkhushboo540@gmail.com>
2024-01-19 15:02:26 +0530

    fixed cors error

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,17 @@ const express = require('express')
 var cors = require('cors');
 const app = express()
 const port = 5000
-
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use(express.json());
 app.use(cors());
+app.use(cors(corsOptions));
 app.use('/api/auth', require('./routes/auth.js'))
 app.use('/api/notes', require('./routes/notes.js'))
 
